d6cfefa9acf166b001cd08c843841d1a051e3c9b
Crowdbotics <team@crowdbotics.com>
2020-12-18 14:38:14 +0000

    remove package blueprint-react-native-sign-in-1-screen

--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -7,7 +7,6 @@ import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
 import Settings171561Navigator from '../features/Settings171561/navigator';
 import Settings171544Navigator from '../features/Settings171544/navigator';
-import SignIn11171497Navigator from '../features/SignIn11171497/navigator';
 
 /**
  * new navigators can be imported here
@@ -18,7 +17,6 @@ const AppNavigator = {
     //@BlueprintNavigationInsertion
 Settings171561: { screen: Settings171561Navigator },
 Settings171544: { screen: Settings171544Navigator },
-SignIn11171497: { screen: SignIn11171497Navigator },
 
     /** new navigators can be added here */
     SplashScreen: {
