60938999b5f9a26e0a9ee8431c37776dc9efb508
Crowdbotics <team@crowdbotics.com>
2020-12-18 15:09:01 +0000

    remove package blueprint-react-native-blank-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -19,7 +19,6 @@ export const installed_blueprints = [
 { name: 'BlankScreen3186584', human_name: 'Blank Screen 3', access_route: 'BlankScreen3186584'},
 { name: 'BlankScreen2186583', human_name: 'Blank Screen 2', access_route: 'BlankScreen2186583'},
 { name: 'BlankScreen1186582', human_name: 'Blank Screen 1', access_route: 'BlankScreen1186582'},
-{ name: 'BlankScreen0186581', human_name: 'Blank Screen 0', access_route: 'BlankScreen0186581'},
 { name: 'Settings171561', human_name: 'Settings', access_route: 'Settings171561'},
 { name: 'Settings171544', human_name: 'Settings', access_route: 'Settings171544'},
 
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -24,7 +24,6 @@ import BlankScreen4186585Navigator from '../features/BlankScreen4186585/navigato
 import BlankScreen3186584Navigator from '../features/BlankScreen3186584/navigator';
 import BlankScreen2186583Navigator from '../features/BlankScreen2186583/navigator';
 import BlankScreen1186582Navigator from '../features/BlankScreen1186582/navigator';
-import BlankScreen0186581Navigator from '../features/BlankScreen0186581/navigator';
 import Settings171561Navigator from '../features/Settings171561/navigator';
 import Settings171544Navigator from '../features/Settings171544/navigator';
 
@@ -54,7 +53,6 @@ BlankScreen4186585: { screen: BlankScreen4186585Navigator },
 BlankScreen3186584: { screen: BlankScreen3186584Navigator },
 BlankScreen2186583: { screen: BlankScreen2186583Navigator },
 BlankScreen1186582: { screen: BlankScreen1186582Navigator },
-BlankScreen0186581: { screen: BlankScreen0186581Navigator },
 Settings171561: { screen: Settings171561Navigator },
 Settings171544: { screen: Settings171544Navigator },
 
