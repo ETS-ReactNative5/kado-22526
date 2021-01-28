7e1f65c6e1cae11977ed06247c2f544b05c4bba2
p8ul <pkinuthia10@gmail.com>
2021-01-28 08:06:26 +0300

    add FAQ functionality

---
+++ backend/faq/api/v1/viewsets.py
@@ -0,0 +1,21 @@
+from rest_framework import authentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import viewsets
+from faq.models import Faq
+from .serializers import FaqSerializer
+
+
+class FAQViewSet(viewsets.ModelViewSet):
+    serializer_class = FaqSerializer
+    authentication_classes = (
+        authentication.TokenAuthentication,
+        authentication.SessionAuthentication
+    )
+    permission_classes = [IsAuthenticated]
+    queryset = Faq.objects.all()
+
+    def get_queryset(self):
+        search_query = self.request.query_params.get('search', None)
+        queryset = Faq.search(search_query)
+        return queryset
