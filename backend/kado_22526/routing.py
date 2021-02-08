1c62bcc817349a8129277b25f768aa3a14d287d6
p8ul <pkinuthia10@gmail.com>
2021-02-08 16:20:09 +0300

    add custom socket auth middleware

--- backend/kado_22526/routing.py
+++ backend/kado_22526/routing.py
@@ -1,9 +1,10 @@
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from chat_user.routing import websocket_urlpatterns
+from core.middlewares import TokenAuthMiddleware
 
 application = ProtocolTypeRouter({
-    "websocket": AuthMiddlewareStack(
+    "websocket": TokenAuthMiddleware(
         URLRouter(
             websocket_urlpatterns
         )
